ed44efa461ccda02b20778dfd2814e9e4123a5b6
P-O <per-olof.edqvst@tele2.se>
2011-06-25 20:50:08 +0200

    First commit

---
+++ src/DagisTimeApp.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author P-O Edqvist
+ *
+ */
+public class DagisTimeApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
