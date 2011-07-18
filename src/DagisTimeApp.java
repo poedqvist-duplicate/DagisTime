590d6ce74d49fca92d01fb3ab78f75cc6a58b5b3
P-O <per-olof.edqvst@tele2.se>
2011-07-18 08:50:31 +0200

    Deleted Person test & class, they where only used to show how to use Git/GitHub with EGit & Eclipse

--- src/DagisTimeApp.java
+++ src/DagisTimeApp.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 /**
  * 
  */
@@ -8,12 +10,35 @@
  */
 public class DagisTimeApp {
 
+	/*
+	 * 2DO
+	 * enter a name of period 1
+	 * enter numberOfDaysInPeriod
+	 * enter a schedule person 1
+	 * enter a schedule person 2
+	 * 
+	 * show the minimum care-time needed
+	 */
+	
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String periodName = JOptionPane.showInputDialog("Namn på period 1");
+		try {
+			int numberOfDaysInPeriod = Integer.parseInt(
+					JOptionPane.showInputDialog("Antal dagar i period 1"));
+		}catch (Exception e){
+			System.out.println(e.getMessage());
+		}
+		
 	}
-
+	
+	/*
+	 * enter nameOfDay
+	 */
+	public static void enterSchedule(){
+		
+	}
 }
